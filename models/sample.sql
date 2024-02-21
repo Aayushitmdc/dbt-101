3d24c6296cb9432854e493bb4c78b2809c4de413
Aayushi Solanki <aayushi.solanki@tmdc.io>
2024-02-21 05:52:33 +0000

    ll

--- models/sample.sql
+++ models/sample.sql
@@ -1,6 +1,6 @@
 
 
 with abcd as
-(SELECT order_id, total_amount as total_amount FROM order_payment group by 1)
+(SELECT order_id, total_amount FROM order_payment group by 1)
 
-select order_id, max(total_amount) from abcd;
+select order_id, max(total_amount) from abcd
