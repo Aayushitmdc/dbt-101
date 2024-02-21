a6bedb7a5f324f16105ce294851de558839aaef1
Aayushi Solanki <aayushi.solanki@tmdc.io>
2024-02-21 05:47:19 +0000

    changed sample.sql

--- models/sample.sql
+++ models/sample.sql
@@ -1,3 +1,6 @@
-{{ config(materialized='table') }}
 
-SELECT order_id, total_amount as total_amount FROM order_payment group by 1
+
+with abcd as
+(SELECT order_id, total_amount as total_amount FROM order_payment group by 1)
+
+select order_id, max(total_amount) from abcd;
