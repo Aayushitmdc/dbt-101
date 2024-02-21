0654121d07e7b8ee48d232128c928be963565020
Aayushi Solanki <aayushi.solanki@tmdc.io>
2024-02-21 06:06:32 +0000

    correct sample.sql working

--- models/sample.sql
+++ models/sample.sql
@@ -1,6 +1,7 @@
+-- with abcd as (
+   select id,
+  concat(first_name, ' ', last_name) as customer_name
+  from {{ source('jaffle_shop', 'customers') }} limit 10
+-- )
 
-
-with abcd as
-(SELECT order_id, total_amount FROM order_payment group by 1)
-
-select order_id, max(total_amount) from abcd
+-- select * from abcd where customer_id > 1
