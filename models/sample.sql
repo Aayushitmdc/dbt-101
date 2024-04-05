d1cd968cc2833b82684ed133704594e0bb8b6a3e
Aayushitmdc <158146401+Aayushitmdc@users.noreply.github.com>
2024-04-05 12:49:24 +0530

    Update sample.sql

--- models/sample.sql
+++ models/sample.sql
@@ -1,8 +1,3 @@
+{{ config(materialized='view') }}
 
-with abcd as (
-   select id,
-  concat(first_name, ' ', last_name) as customer_name
-  from {{ source('jaffle_shop', 'customers') }} limit 100
-)
-
-select * from abcd where id > 1
+SELECT first_name, round(avg(age),2) as mean_age FROM customer group by 1
